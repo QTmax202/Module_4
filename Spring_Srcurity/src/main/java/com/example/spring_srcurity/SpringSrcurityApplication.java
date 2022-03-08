d9eff2ed10a9f193c88f7e56e2f70bb521e55fea
QTmax202 <89342545+QTmax202@users.noreply.github.com>
2022-03-08 21:37:50 +0700

    08/03/2022

---
+++ Spring_Srcurity/src/main/java/com/example/spring_srcurity/SpringSrcurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_srcurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSrcurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSrcurityApplication.class, args);
+    }
+
+}
